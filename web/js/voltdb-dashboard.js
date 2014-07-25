6a48d4c7c77923c62888cf7c09ed5d570651235d
Ning Shi <nshi@voltdb.com>
2014-07-25 16:14:34 -0400

    Show times under the charts in local time.
    Limit the number of ticks in the charts so that they don't overlap each other.

--- web/js/voltdb-dashboard.js
+++ web/js/voltdb-dashboard.js
@@ -136,7 +136,7 @@ function DrawTPSChart(response, someDiv) {
 	    //bars: { show: true, barWidth : 60*1000, fill: true},
 	    points: { show: false }
         },
-        xaxis: { mode: "time" },
+        xaxis: { mode: "time", timezone: "browser", minTickSize: [10, "second"], ticks: 4 },
         yaxis: { position: "right" },
         legend: { position: 'nw' }
     };
