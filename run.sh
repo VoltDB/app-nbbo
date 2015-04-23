76272ab1775dbe74c610e6d702d4ce178eb20ca3
Ben Ballard <benjaminballard@gmail.com>
2015-04-23 12:27:54 -0400

    5.x updates

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -o errexit #exit on any single command fail
+
 # VoltDB variables
 APPNAME="nbbo"
 HOST=localhost
@@ -11,81 +13,33 @@ WEB_PORT=8081
 # CLIENT variables
 SERVERS=localhost
 
-
-# Get the PID from PIDFILE if we don't have one yet.
-if [[ -z "${PID}" && -e web/http.pid ]]; then
-  PID=$(cat web/http.pid);
-fi
-
-# find voltdb binaries in either installation or distribution directory.
-# if [ -n "$(which voltdb 2> /dev/null)" ]; then
-#     VOLTDB_BIN=$(dirname "$(which voltdb)")
-# else
-#     VOLTDB_BIN="$(pwd)/../../../bin"
-#     echo "The VoltDB scripts are not in your PATH."
-#     echo "For ease of use, add the VoltDB bin directory: "
-#     echo
-#     echo $VOLTDB_BIN
-#     echo
-#     echo "to your PATH."
-#     echo
-# fi
-# installation layout has all libraries in $VOLTDB_ROOT/lib/voltdb
-# if [ -d "$VOLTDB_BIN/../lib/voltdb" ]; then
-#     VOLTDB_BASE=$(dirname "$VOLTDB_BIN")
-#     VOLTDB_LIB="$VOLTDB_BASE/lib/voltdb"
-#     VOLTDB_VOLTDB="$VOLTDB_LIB"
-# distribution layout has libraries in separate lib and voltdb directories
-# else
-#     VOLTDB_BASE=$(dirname "$VOLTDB_BIN")
-#     VOLTDB_LIB="$VOLTDB_BASE/lib"
-#     VOLTDB_VOLTDB="$VOLTDB_BASE/voltdb"
-# fi
-
-# APPCLASSPATH=$CLASSPATH:$({ \
-#     \ls -1 "$VOLTDB_VOLTDB"/voltdb-*.jar; \
-#     \ls -1 "$VOLTDB_LIB"/*.jar; \
-#     \ls -1 "$VOLTDB_LIB"/extension/*.jar; \
-# } 2> /dev/null | paste -sd ':' - )
-# VOLTDB="$VOLTDB_BIN/voltdb"
-# LOG4J="$VOLTDB_VOLTDB/log4j.xml"
-
 # This script assumes voltdb/bin is in your path
 VOLTDB_HOME=$(dirname $(dirname "$(which voltdb)"))
 
-LICENSE="$VOLTDB_HOME/voltdb/license.xml"
 
+function start_web() {
+    stop_web
+    nohup python -m SimpleHTTPServer $WEB_PORT > web/http.log 2>&1 &
+    echo "started demo http server"
+}
+
+function stop_web() {
+    WEB_PID=$(ps -ef | grep "SimpleHTTPServer $WEB_PORT" | grep python | awk '{print $2}')
+    if [[ ! -z "$WEB_PID" ]]; then
+        kill $WEB_PID
+        echo "stopped demo http server"
+    fi
+}
 
 
 # remove non-source files
 function clean() {
     rm -rf voltdbroot statement-plans log catalog-report.html
-    rm -f web/http.log web/http.pid
+    rm -f web/http.log
     rm -rf db/obj db/$APPNAME.jar db/nohup.log
     rm -rf client/obj client/log
 }
 
-function start_web() {
-    if [[ -z "${PID}" ]]; then
-        cd web
-        nohup python -m SimpleHTTPServer $WEB_PORT > http.log 2>&1 &
-        echo $! > http.pid
-        cd ..
-        echo "started http server"
-    else
-        echo "http server is already running (PID: ${PID})"
-    fi
-}
-
-function stop_web() {
-  if [[ -z "${PID}" ]]; then
-    echo "http server is not running (missing PID)."
-  else
-      kill ${PID}
-      rm web/http.pid
-      echo "stopped http server (PID: ${PID})."
-  fi
-}
 
 # compile any java stored procedures
 function compile_procedures() {
@@ -99,7 +53,13 @@ function compile_procedures() {
     fi
 }
 
-# build an application catalog
+function procedure_jar() {
+    compile_procedures
+    jar cf db/procs.jar -C db/obj .
+}
+
+
+# DEPRECATED - build an application catalog 
 function catalog() {
     compile_procedures
     voltdb compile --classpath db/obj -o db/$APPNAME.jar db/ddl.sql
@@ -116,11 +76,22 @@ function server() {
     tail -f db/nohup.log
 }
 
+function nohup_server_no_catalog() {
+    nohup voltdb create -d db/$DEPLOYMENT -H $HOST > db/nohup.log 2>&1 &
+}
+
+function init() {
+    procedure_jar
+    cd db
+    sqlcmd < ddl.sql
+    cd ..
+}
+
 function nohup_server() {
     # if a catalog doesn't exist, build one
     if [ ! -f "db/$APPNAME.jar" ]; then catalog; fi
     # run the server
-    nohup voltdb create -d db/$DEPLOYMENT -l $LICENSE -H $HOST db/$APPNAME.jar > db/nohup.log 2>&1 &
+    nohup voltdb create -d db/$DEPLOYMENT -H $HOST db/$APPNAME.jar > db/nohup.log 2>&1 &
 }
 
 function cluster-server() {
